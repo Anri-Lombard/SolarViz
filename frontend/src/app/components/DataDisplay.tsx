0e34fe0f739f27a44ed94bf68e10873c86fbd842
Anri Lombard <anri.m.lombard@gmail.com>
2023-08-03 09:43:58 +0200

    Template

--- frontend/src/app/components/DataDisplay.tsx
+++ frontend/src/app/components/DataDisplay.tsx
@@ -3,13 +3,14 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 
 interface DataType {
   Timestamp: string;
-  'UCT - DSchool - Basics - UCT - DSchool Load Power [W] - P_LOAD': string;
-  'UCT - DSchool - Basics - UCT - DSchool Solar [W] - P_SOLAR': string;
-  'UCT - DSchool - Basics - UCT - DSchool Incomer Power [W] - P_INCOMER': string;
+  'Load Power [W]': string;
+  'Solar [W]': string;
+  'Incomer Power [W]': string;
 }
 
 export default function DataDisplay() {
   const [data, setData] = useState<DataType[] | null>(null);
+  const [showSingleChart, setShowSingleChart] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,29 +22,95 @@ export default function DataDisplay() {
     fetchData();
   }, []);
 
+  const toggleChartDisplay = () => {
+    setShowSingleChart(!showSingleChart);
+  };
+
   return (
-    <div style={{ width: '100%', height: 500 }}>
+    <div style={{ width: '100%', height: 'auto' }}>
+      <button onClick={toggleChartDisplay}>
+        {showSingleChart ? 'Show Multiple Charts' : 'Show Single Chart'}
+      </button>
       {data ? (
-        <ResponsiveContainer>
-          <LineChart
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="Timestamp" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line type="monotone" dataKey="UCT - DSchool - Basics - UCT - DSchool Load Power [W] - P_LOAD" stroke="#8884d8" activeDot={{ r: 8 }} />
-            <Line type="monotone" dataKey="UCT - DSchool - Basics - UCT - DSchool Solar [W] - P_SOLAR" stroke="#82ca9d" />
-            <Line type="monotone" dataKey="UCT - DSchool - Basics - UCT - DSchool Incomer Power [W] - P_INCOMER" stroke="#ffc658" />
-          </LineChart>
-        </ResponsiveContainer>
+        showSingleChart ? (
+          <ResponsiveContainer height={500}>
+            <LineChart
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="Timestamp" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="UCT - DSchool - Basics - UCT - DSchool Load Power [W] - P_LOAD" stroke="#8884d8" dot={false} />
+              <Line type="monotone" dataKey="UCT - DSchool - Basics - UCT - DSchool Solar [W] - P_SOLAR" stroke="#82ca9d" dot={false} />
+              <Line type="monotone" dataKey="UCT - DSchool - Basics - UCT - DSchool Incomer Power [W] - P_INCOMER" stroke="#ffc658" dot={false} />
+            </LineChart>
+          </ResponsiveContainer>
+        ) : (
+          <>
+            <ResponsiveContainer height={500}>
+              <LineChart
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="Timestamp" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="UCT - DSchool - Basics - UCT - DSchool Load Power [W] - P_LOAD" stroke="#8884d8" dot={false} />
+              </LineChart>
+            </ResponsiveContainer>
+            <ResponsiveContainer height={500}>
+              <LineChart
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="Timestamp" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="UCT - DSchool - Basics - UCT - DSchool Solar [W] - P_SOLAR" stroke="#82ca9d" dot={false} />
+              </LineChart>
+            </ResponsiveContainer>
+            <ResponsiveContainer height={500}>
+              <LineChart
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="Timestamp" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="UCT - DSchool - Basics - UCT - DSchool Incomer Power [W] - P_INCOMER" stroke="#ffc658" dot={false} />
+              </LineChart>
+            </ResponsiveContainer>
+          </>
+        )
       ) : (
         <p>Loading...</p>
       )}
