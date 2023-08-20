99f7f7c5332b5d9a4ce1d2e8efbca8b7268b475b
Anri Lombard <anri.m.lombard@gmail.com>
2023-08-20 07:03:00 +0200

    Tailwind styling and manual styling

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -3,21 +3,18 @@
 import React from 'react';
 import { useSettings } from '../contexts/SettingsContext';
 
-import '../styles/Settings.css'
-
 type ColorType = 'incomerPower' | 'solarPower' | 'water';
 
 export default function Settings() {
   const { settings, setSettings } = useSettings();
 
-  const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'black', 'white', 'gray', 'cyan', 'magenta', 'maroon', 'navy', 'olive', 'teal', 'lime', 'aqua', 'fuchsia', 'silver', 'gold', 'orange'];
+  const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'black', 'white', 'gray', 'cyan', 'magenta', 'maroon', 'navy', 'olive', 'teal', 'lime', 'aqua', 'fuchsia', 'silver', 'gold', 'orange']; 
 
   const defaultColors = {
     incomerPower: '#183d33',
     solarPower: '#bd5545',
     water: '#2779a7',
   };
-
 
   const handleChangeColor = (type: ColorType, color: string) => {
     setSettings({
@@ -29,18 +26,17 @@ export default function Settings() {
   };
 
   const renderColorOptions = (type: ColorType) => (
-    <div className="flex">
+    <div className="flex flex-col mb-5">
       <h3 className='text-black'>{type} Color</h3>
       <div className="flex flex-wrap">
         {colors.map((color) => (
-          <div key={color} className="m-1">
-            <button
-              onClick={() => handleChangeColor(type, color)}
-              className={`p-2 ${settings[type] === color ? 'bg-blue-500 text-white' : 'bg-gray-400'}`}
-            >
-              {color.charAt(0).toUpperCase() + color.slice(1)}
-            </button>
-          </div>
+          <button
+            key={color}
+            onClick={() => handleChangeColor(type, color)}
+            className={`p-2 m-1 ${settings[type as keyof typeof settings] === color ? 'bg-blue-500 text-white' : 'bg-gray-400 text-black'}`}
+          >
+            {color.charAt(0).toUpperCase() + color.slice(1)}
+          </button>
         ))}
       </div>
     </div>
@@ -55,25 +51,19 @@ export default function Settings() {
         {Object.entries(defaultColors).map(([type, color]) => (
           <div key={type} className="flex items-center mb-2">
             <span className='text-black mr-2'>{type}: </span>
-            <button
+            <button 
               onClick={() => handleChangeColor(type as ColorType, color)}
               className={`p-2 ${settings[type as keyof typeof settings] === color ? 'bg-blue-500 text-white' : 'bg-gray-400 text-black'}`}
-              style={{ backgroundColor: color }}
+              style={{backgroundColor: color}}
             >
-              {color.charAt(0).toUpperCase() + color.slice(1)}
+              {color}
             </button>
           </div>
         ))}
       </div>
-      <div className='mb-5'>
-        {renderColorOptions('incomerPower')}
-      </div>
-      <div className='mb-5'>
-        {renderColorOptions('solarPower')}
-      </div>
-      <div className='mb-5'>
-        {renderColorOptions('water')}
-      </div>
+      {renderColorOptions('incomerPower')}
+      {renderColorOptions('solarPower')}
+      {renderColorOptions('water')}
     </div>
   );
 }
