bd228ec9867ff0bd1daf54e0371c803c76ce2bd8
biancaacafrao <acfbia001@myuct.ac.za>
2023-08-25 13:34:19 +0200

    when user clicks enter on keyboard, login is pressed

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -20,15 +20,34 @@ const LoginPage = () => {
     } catch (error) {
       console.error('Login failed:', error);
       setError('Incorrect username or password. Try again.')
-      // TODO: Handle login failure (e.g., show an error message)
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      login();
     }
   };
 
   return (
     <div>
-      {error && <div className="text-red-500 bm-3">{error}</div>}
-      <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className="mt-5 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
+      {error && <div className="text-red-500 bm-3"
+        >{error}
+      </div>}
+
+      <input
+        type="text" 
+        placeholder="Username" 
+        onChange={(e) => setUsername(e.target.value)} 
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+
+      <input type="password" 
+        placeholder="Password" 
+        onChange={(e) => setPassword(e.target.value)}
+        onKeyDown={handleKeyDown}
+        className="mt-5 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+      />
+      
       <button onClick={login} className="mt-5 bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mx-auto block">Login</button>
     </div>
   );
