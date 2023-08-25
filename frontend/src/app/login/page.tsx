865cd211dc48c9d0c77fa73de49e1b02616fafe4
biancaacafrao <acfbia001@myuct.ac.za>
2023-08-25 11:46:33 +0200

    Added error message for when user inputs incorrec login

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -7,22 +7,26 @@ import { useRouter } from 'next/navigation';
 const LoginPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error,setError] = useState('');// state for error message
   const router = useRouter();
 
   const login = async () => {
     try {
+      setError(''); //clear previous error message
       const response = await axios.post('http://localhost:8000/api-token-auth/', { username, password });
       const token = response.data.token;
       localStorage.setItem('token', token);
       router.push('login/admin/');
     } catch (error) {
       console.error('Login failed:', error);
+      setError('Incorrect username or password. Try again.')
       // TODO: Handle login failure (e.g., show an error message)
     }
   };
 
   return (
     <div>
+      {error && <div className="text-red-500 bm-3">{error}</div>}
       <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
       <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} className="mt-5 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" />
       <button onClick={login} className="mt-5 bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mx-auto block">Login</button>
