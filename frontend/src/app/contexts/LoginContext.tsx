00dfe0cad277c4fcecad2a92b76ffce2418fa3f7
biancaacafrao <acfbia001@myuct.ac.za>
2023-09-01 19:23:46 +0200

    testing something

---
+++ frontend/src/app/contexts/LoginContext.tsx
@@ -0,0 +1,36 @@
+// LoginContext.tsx
+import React, { createContext, useContext, useState } from 'react';
+
+interface AuthContextType {
+  isLoggedIn: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}
