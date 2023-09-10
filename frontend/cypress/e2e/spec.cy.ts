5a1a11085b4faa97cfe5b324577625c8c8166bb5
Anri Lombard <anri.m.lombard@gmail.com>
2023-09-10 14:24:04 +0200

    Initial tests

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,5 +1,25 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Home Page', () => {
+  it('should visit the home page', () => {
+    cy.visit('http://localhost:3000/');
+  });
+});
+
+describe('moreData Page', () => {
+  it('should visit the moreData page', () => {
+    cy.visit('http://localhost:3000/moreData');
+  });
+});
+
+describe('login Page', () => {
+  it('should visit the login page', () => {
+    cy.visit('http://localhost:3000/login');
+  });
+}
+);
+
+describe('manual Page', () => {
+  it('should visit the manual page', () => {
+    cy.visit('http://localhost:3000/manual');
+  });
+}
+);
