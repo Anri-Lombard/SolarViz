aca476936926dfad0dd1c21ae32690acf95f295a
biancaacafrao <acfbia001@myuct.ac.za>
2023-09-10 17:13:40 +0200

    sequence and duration become unclickable when graph is not displayed. Need to fix sequence issue

--- frontend/src/app/components/GraphSettings.tsx
+++ frontend/src/app/components/GraphSettings.tsx
@@ -1,36 +1,45 @@
-// GraphSettingsComponent.tsx
-
 import React from 'react';
 import { GraphSettingsProps } from '../types/dataTypes';
 
-const GraphSettingsComponent: React.FC<GraphSettingsProps> = ({ chartType, handleGraphSettingsChange, settings }) => (
-  <div className="gridElement">
-    <h3 className="font-bold text-l">{chartType}</h3>
-    <label>
-      Sequence:
-      <input
-        type="number"
-        value={settings[chartType].sequence}
-        onChange={(e) => handleGraphSettingsChange(chartType, 'sequence', parseInt(e.target.value))}
-      />
-    </label>
-    <label>
-      Duration (seconds):
-      <input
-        type="number"
-        value={settings[chartType].duration}
-        onChange={(e) => handleGraphSettingsChange(chartType, 'duration', parseInt(e.target.value))}
-      />
-    </label>
-    <label className='checkbox'>
-      Display:
-      <input
-        type="checkbox"
-        checked={settings[chartType].display}
-        onChange={(e) => handleGraphSettingsChange(chartType, 'display', e.target.checked)}
-      />
-    </label>
-  </div>
-);
+const GraphSettingsComponent: React.FC<GraphSettingsProps> = ({ chartType, handleGraphSettingsChange, settings }) => {
+
+  const handleDisplayChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const isChecked = e.target.checked;
+    const newSequence = isChecked ? settings[chartType].sequence : null; // Set sequence to null if not checked
+    handleGraphSettingsChange(chartType, 'display', newSequence);
+  };
+
+  return (
+    <div className="gridElement">
+      <h3 className="font-bold text-l">{chartType}</h3>
+      <label>
+        Sequence:
+        <input
+          type="number"
+          value={settings[chartType].sequence}
+          onChange={(e) => handleGraphSettingsChange(chartType, 'sequence', parseInt(e.target.value))}
+          disabled={!settings[chartType].display} // Disable if 'Display' is unchecked
+        />
+      </label>
+      <label>
+        Duration (seconds):
+        <input
+          type="number"
+          value={settings[chartType].duration}
+          onChange={(e) => handleGraphSettingsChange(chartType, 'duration', parseInt(e.target.value))}
+          disabled={!settings[chartType].display} // Disable if 'Display' is unchecked
+        />
+      </label>
+      <label className='checkbox'>
+        Display:
+        <input
+          type="checkbox"
+          checked={settings[chartType].display}
+          onChange={handleDisplayChange}
+        />
+      </label>
+    </div>
+  );
+}
 
 export default GraphSettingsComponent;
