c88ba9e722adf67bebebc112319dc5420453e1e5
biancaacafrao <acfbia001@myuct.ac.za>
2023-09-16 14:01:21 +0200

    Revert "Merge branch 'main' of https://github.com/Anri-Lombard/SolarViz"
    This reverts commit 1192bb808ef8db5cdf7565a7dd69260ee40cb8d3, reversing
    changes made to cb7f99a722f4a5b4db90797c9d4cfe3125a6ad5d.

--- frontend/src/app/components/StackedAreaChart.tsx
+++ frontend/src/app/components/StackedAreaChart.tsx
@@ -1,9 +1,6 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { AreaChart, Area, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { StackedAreaChartProps } from '../types/chartTypes';
-import { getTickFormatter } from '../utils/DataUtils';
-
-import LoadingSpinner from './LoadingSpinner';
 
 /**
  * StackedAreaChart displays a stacked area chart using Recharts library.
@@ -13,49 +10,20 @@ import LoadingSpinner from './LoadingSpinner';
  * @param {Object} props.colors             Colors for chart areas.
  * @returns {JSX.Element}                   The StackedAreaChart JSX.
  */
-export const StackedAreaChart: React.FC<StackedAreaChartProps> = ({ data, colors, selectedPowerType, showForecast, duration }) => {
-    const tickFormatter = useMemo(() => getTickFormatter(duration), [duration]);
-    
-    if (!data || data.length === 0) {
-        return <LoadingSpinner />;
-    }
-    
+
+export const StackedAreaChart: React.FC<StackedAreaChartProps> = ({ data, colors, selectedPowerType}) => {
     // Convert power data to kWh assuming the data is already aggregated per hour
     const convertedData = data.map(item => ({
-        Timestamp: new Date(item.Timestamp),
+        Timestamp: item.Timestamp,
         'Load Power': parseFloat(item['Load Power']) / 1000,
         'Solar Power': parseFloat(item['Solar Power']) / 1000,
         'Incomer Power': parseFloat(item['Incomer Power']) / 1000,
-        'Expected Power': parseFloat(item['Expected Power']) / 1000,
     }));
 
-    // Sort data by Timestamp
-    convertedData.sort((a, b) => a.Timestamp.getTime() - b.Timestamp.getTime());
-
-    // Identify the last timestamp
-    const lastTimestamp = new Date(convertedData[convertedData.length - 1].Timestamp);
-
-    // Filter data based on duration and last timestamp
-    const filteredData = convertedData.filter(item => {
-        if (duration === 'day') {
-            return item.Timestamp.getDate() === lastTimestamp.getDate() &&
-                   item.Timestamp.getMonth() === lastTimestamp.getMonth() &&
-                   item.Timestamp.getFullYear() === lastTimestamp.getFullYear();
-        } else if (duration === 'month') {
-            return item.Timestamp.getMonth() === lastTimestamp.getMonth() &&
-                   item.Timestamp.getFullYear() === lastTimestamp.getFullYear();
-        } else if (duration === 'year') {
-            return item.Timestamp.getFullYear() === lastTimestamp.getFullYear();
-        }
-        return true;
-    });
-
-    // FIXME: X-axis
-    
     return (
         <ResponsiveContainer height={600}>
             <AreaChart
-                data={filteredData}
+                data={convertedData}
                 margin={{
                     top: 10,
                     right: 30,
@@ -63,13 +31,12 @@ export const StackedAreaChart: React.FC<StackedAreaChartProps> = ({ data, colors
                     bottom: 20,
                 }}
             >
-                <XAxis
-                    dataKey={(d) => d.Timestamp.getTime()}  // Convert Date to timestamp
-                    tickFormatter={tickFormatter}
+                <XAxis 
+                    dataKey="Timestamp" 
                     label={{ value: 'Date and Hour', position: 'bottom' }}
                 />
-                <YAxis
-                    label={{ value: 'Usage (kW)', angle: -90, position: 'insideLeft', offset: -10 }}
+                <YAxis 
+                    label={{ value: 'Usage (kW)', angle: -90, position: 'insideLeft', offset: -10 }} 
                 />
                 <Tooltip
                     formatter={(value: number, name: string) => {
@@ -80,7 +47,6 @@ export const StackedAreaChart: React.FC<StackedAreaChartProps> = ({ data, colors
                 {selectedPowerType === 'All' || selectedPowerType === 'Load Power' ? <Area type="monotone" dataKey="Load Power" stackId="1" stroke="#000" fill="none" strokeWidth={2} /> : null}
                 {selectedPowerType === 'All' || selectedPowerType === 'Incomer Power' ? <Area type="monotone" dataKey="Incomer Power" stackId="2" stroke={colors.incomerPower} fill={colors.incomerPower} /> : null}
                 {selectedPowerType === 'All' || selectedPowerType === 'Solar Power' ? <Area type="monotone" dataKey="Solar Power" stackId="3" stroke={colors.solarPower} fill={colors.solarPower} /> : null}
-                {showForecast ? <Area type="monotone" dataKey="Expected Power" stackId="4" stroke="#999" fill="#fff" strokeWidth={2} /> : null}
             </AreaChart>
         </ResponsiveContainer>
     );
